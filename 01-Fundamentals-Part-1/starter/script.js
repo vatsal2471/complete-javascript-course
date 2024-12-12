00df6525302c2a04bfe1b401eaf150c8fc497709
vatsal thakkar <vatsal247@gmail.com>
2024-12-12 16:35:23 -0600

    updated for data Type

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -13,3 +14,26 @@ let myFirstJob = "Programmer";
 let myCurrentJob = "teacher";
 
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES!";
+
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+console.log(year);
+
+console.log(typeof null);

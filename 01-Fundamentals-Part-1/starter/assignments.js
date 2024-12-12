999dff7e3c2e1f7081323a0ffaaf63bcc54f1102
vatsal thakkar <vatsal247@gmail.com>
2024-12-12 15:57:51 -0600

    new js file

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "USA";
+let continent = "North America";
+let population = "300 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,15 @@
 let js = "amazing";
-if ((js = "amazing")) alert("Javascript is fun");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log("23");
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "teacher";
+
+console.log(myFirstJob);
